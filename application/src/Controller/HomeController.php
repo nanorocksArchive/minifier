ba624841cd7f9f82010e5970991c98a63791b1ac
nanorocks <andrejnankov@gmail.com>
2019-06-14 17:23:47 +0200

    WIP need to implement minifer here

--- application/src/Controller/HomeController.php
+++ application/src/Controller/HomeController.php
@@ -4,19 +4,54 @@ use App\Model\MinifyCss;
 
 class HomeController
 {
+    /**
+     * @var string fileType
+     */
+    protected static $fileType = 'css';
 
+    /**
+     * Render home page
+     */
     public function home()
     {
-        Flight::render('home/home.html.php', array('name' => 'Bob'));
+        Flight::render('home/home.html.twig');
     }
 
+    /**
+     * Render processed css
+     */
     public function css()
     {
-        echo "css page!!!";
+        self::processFile('css');
     }
 
+    /**
+     * Render processed js
+     */
     public function js()
     {
-        echo "js page!!!";
+        self::processFile('js');
+    }
+
+    /**
+     * Processing css & js
+     */
+    public static function processFile($fileType)
+    {
+        $request = Flight::request();
+
+        $OldContent = $request->data['content'];
+        $type = $request->data['type'];
+
+        if (strtolower($type) != $fileType) {
+            Flight::render('home/home.html.twig', ['error' => "Invalid type, only {$fileType} allowed."]);
+        }
+
+        // TODO add minifier here ...
+
+        $NewContent = $OldContent;
+
+        Flight::render('home/home.html.twig', ['NewContent' => $NewContent, 'OldContent' => $OldContent]);
+
     }
 }
