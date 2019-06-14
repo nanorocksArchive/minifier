9f54f6c5f0f1d5d01152155d4e4f9a02b70da4e8
nanorocks <andrejnankov@gmail.com>
2019-06-14 17:18:24 +0200

    ADD twig template engine to project

--- application/src/dependencies.php
+++ application/src/dependencies.php
@@ -4,8 +4,22 @@
  * Dependencies for routes, views ...
  */
 
-Flight::set('flight.views.path', __DIR__ . '/../views');
+// Set twig as template engine with FLight
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/views');
+Flight::register(
+    'view',
+    'Twig_Environment',
+    array($loader, array('debug' => true)),
+    function($twig) {
+        // Call back that takes instance as arugment
+        // Included to show how to configure twig
+    }
+);
+Flight::map('render', function($template, $data=array()) {
+    Flight::view()->display($template, $data);
+});
 
-Flight::set('flight.base_url', '/../public');
+// Add to false on production
+Flight::set('flight.log_errors', true);
 
 require_once __DIR__ . '/routes.php';
